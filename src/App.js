361c96689e0be7d0d7963c0161dcc9ed7c43a5fb
bfinell <rsgod123>
2020-12-05 11:16:28 +0200

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import AddRecipe from "./components/AddRecipe";
 
-function App() {
-  return (
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            recipes:[
+                this.name = "test"
+
+            ]
+        }
+    }
+
+
+    handleAddRecipe(recipe){
+        let recipes = this.state.recipes;
+        recipes.push(recipe)
+        this.setState({recipes:recipes});
+    }
+
+
+    render() {
+
+    console.log(this.state.recipes)
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <AddRecipe addRecipe={this.handleAddRecipe.bind(this)}/>
+1        {this.state.recipes.map(function(recipe, idx){
+         return (<li key={idx}>{recipe.name}</li>)
+                })}
+
     </div>
   );
+    }
+
+
 }
+
 
 export default App;
